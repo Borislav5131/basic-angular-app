224381dbc85e6df97d450986dda11edce9362908
Borislav Yurkev <borislavurkev@gmail.com>
2022-06-07 18:07:24 +0300

    Change emitters with subject

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from './shopping-list.service';
 
@@ -7,16 +9,21 @@ import { ShoppingListService } from './shopping-list.service';
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
-export class ShoppingListComponent implements OnInit {
+export class ShoppingListComponent implements OnInit, OnDestroy {
   ingredients: Ingredient[];
+  private igChangeSubscription: Subscription;
 
   constructor(private shoppingListService:ShoppingListService) { }
 
   ngOnInit(): void {
     this.ingredients = this.shoppingListService.getIngredients();
-    this.shoppingListService.ingredientsChanged
+    this.igChangeSubscription = this.shoppingListService.ingredientsChanged
       .subscribe((ingredients: Ingredient[]) =>{
         this.ingredients = ingredients
       });
+  }
+
+  ngOnDestroy(): void {
+      this.igChangeSubscription.unsubscribe();
   }
 }
